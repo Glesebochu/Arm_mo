d71dd1c5b6cfd74ddad97dbeaf4f04670b16b959
FinhasYohannesGustavo <pj.gustavojhon@gmail.com>
2024-01-25 07:14:37 +0300

    Users can now login with thier created accounts!

--- Back-end/login.php
+++ Back-end/login.php
@@ -4,13 +4,22 @@
 // Retrieve the submitted form data
 $username = $_POST['username'];
 $password = $_POST['password'];
+include_once('Connect.php');
+$con = new Connect;
+$db = $con->__getConnection();
+$db->query('USE Arm_mo');
+$queryCheckCredentials = "CALL CheckCredentials('$username', '$password', @p_exists)";
+$db->query($queryCheckCredentials);
+$result = $db->query("SELECT @p_exists");
+$row = $result->fetch_assoc();
+$count = $row['@p_exists'];
 
-if ($username === 'admin@gmail.com' && $password === 'password') {
+if ($count == 1) {
   header('Location: ../Front-end/home-settings/home-page.html');
   exit;
 } else {
-   $errorMessage = 'Invalid username or password';  
-   header("Location: ../Front-end/About_us-login-signup/login.html?error=".urlencode($errorMessage));
-   exit;
+  $errorMessage = 'Invalid username or password';  
+  header("Location: ../Front-end/About_us-login-signup/login.html?error=" . urlencode($errorMessage));
+  exit;
 }
 ?>

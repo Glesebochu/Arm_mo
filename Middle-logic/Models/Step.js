f70658314694592a4e6059043b400cb3518f29b0
Zelalem Amare <fellasfaw@outlook.com>
2024-01-31 23:22:26 +0300

    Added StepCategory and StepType classes.

--- Middle-logic/Models/Step.js
+++ Middle-logic/Models/Step.js
@@ -1,3 +1,12 @@
+class StepCategory{
+    static Preparation = "Preparation";
+    static Transition = "Transition";
+}
+class StepType{
+    static Instruction = "Instruction";
+    static Question = "Question";
+}
+
 class Step{
     constructor(title, description, category, type){
         this.Title = title;
@@ -6,68 +15,51 @@ class Step{
         this.Type = type;
     }
     
-    static getDummySteps(){
-        const Categories = {
-            Preparation: "Preparation",
-            Transition: "Transition"
-        };
-        const Types = {
-            Instruction: "Instruction",
-            Question: "Question"
-        };
+    static getDummyPreparationSteps(){
 
         var preparationSteps = [
             new Step(
                 "Selection", 
                 "Write down the activity you choose to do.", 
-                Categories.Preparation, 
-                Types.Question
+                StepCategory.Preparation, 
+                StepType.Question
             ),
             new Step(
                 "Motivation",
                 "Review your purpose for doing this activity. Don't judge your reasons. Be aware and accept them.", 
-                Categories.Preparation, 
-                Types.Question
+                StepCategory.Preparation, 
+                StepType.Question
             ),
             new Step(
                 "Goal",
                 "Decide on what you hope to work on in this session. Keep it simple and small.", 
-                Categories.Preparation, 
-                Types.Question
+                StepCategory.Preparation, 
+                StepType.Question
             ),
             new Step(
                 "Expectations",
                 "Remember the dangers of expecting unreasonably and too highly. Be gentle with yourself.", 
-                Categories.Preparation, 
-                Types.Instruction
+                StepCategory.Preparation, 
+                StepType.Instruction
             ),
             new Step(
                 "Diligence",
                 "Resolve to practice diligently for the entire session no matter how it goes. Write it down.", 
-                Categories.Preparation, 
-                Types.Question
+                StepCategory.Preparation, 
+                StepType.Question
             ),
             new Step(
                 "Distractions",
                 "Write down thoughts, emotions, ideas, plans, worries or any other distractions that may arise. Resolve to set them aside if they arise in the middle of the activity.", 
-                Categories.Preparation, 
-                Types.Question
+                StepCategory.Preparation, 
+                StepType.Question
             ),
             new Step(
                 "Posture",
                 "Make yourself as comfortable as possible.", 
                 "", 
-                Categories.Preparation, 
-                Types.Instruction
-            )
-        ];
-
-        var transitionSteps = [
-            new Step(
-                "Everything",
-                "Take in all sensory stimuli as they occur in the present",
-                Categories.Transition,
-                Types.Instruction
+                StepCategory.Preparation, 
+                StepType.Instruction
             )
         ];
 

b236141e7f6a58db2a0a2b1000a8e360a92dff1b
Fellasfaw <fellasfaw@outlook.com>
2024-02-02 00:16:05 +0300

    Removed `email` property from `Meditator`.

--- Middle-logic/Models/Meditator.js
+++ Middle-logic/Models/Meditator.js
@@ -2,11 +2,10 @@ import { prepare } from "../../Front-end/Game/scripts/preparation.js";
 import { Session } from "./Session.js";
 
 export class Meditator{
-    constructor(firstName, lastName, username, email, password, currentStage){
+    constructor(firstName, lastName, username, password, currentStage){
         this.First_Name = firstName;
         this.Last_Name = lastName;
         this.Username = username;
-        this.Email = email;
         this.Password = password;
         this.Current_Stage = currentStage;
     }

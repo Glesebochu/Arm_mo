bf359f0e1adf1f8307b6822f25733b61b3ee926d
Fellasfaw <fellasfaw@outlook.com>
2024-02-02 02:48:30 +0300

    Replaced `Current_Stage` with `Current_Stage_No` in `Meditator`.

--- Middle-logic/Models/Meditator.js
+++ Middle-logic/Models/Meditator.js
@@ -1,9 +1,9 @@
 export class Meditator{
-    constructor(firstName, lastName, username, password, currentStage){
+    constructor(firstName, lastName, username, password, currentStageNo){
         this.First_Name = firstName;
         this.Last_Name = lastName;
         this.Username = username;
         this.Password = password;
-        this.Current_Stage = currentStage;
+        this.Current_Stage_No = currentStageNo;
     }
 }

48161e2201bd753cd9d0d1f606c0b6b165bc3288
Fellasfaw <fellasfaw@outlook.com>
2024-02-02 02:09:10 +0300

    Added dummy Obstacles until stage 3.

--- Middle-logic/Models/Obstacle.js
+++ Middle-logic/Models/Obstacle.js
@@ -1,5 +1,19 @@
-export class Obstacle{
-    constructor(description){
+export class Obstacle {
+    constructor(description) {
         this.Description = description;
     }
+
+    static Resistance = "Resistance";
+    static Procrastination = "Procrastination";
+    static Fatigue = "Fatigue";
+    static Impatience = "Impatience";
+    static Boredom = "Boredom";
+    static Motivation_Lack = "Lack of motivation";
+    static Mind_Wandering = "Mind wandering";
+    static Monkey_Mind = "Monkey mind";
+    static Impatience = "Impatience";
+    static Distractions = "Distractions";
+    static Forgetting = "Forgetting";
+    static Sleepiness = "Sleepiness";
+
 }

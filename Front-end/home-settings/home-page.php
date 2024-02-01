4b2a312c3692d0cb73efa3f509d31046a70254de
Fellasfaw <fellasfaw@outlook.com>
2024-02-01 16:18:32 +0300

    Modified some back-end code with Gustav.

--- Front-end/home-settings/home-page.php
+++ Front-end/home-settings/home-page.php
@@ -8,7 +8,8 @@
   <title>Home Page</title>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mako%3A400"/>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro%3A400"/>
-  <link rel="stylesheet" href="./styles/home-page.css"/>
+  <!-- <link rel="stylesheet" href="./styles/home-page.css"/> -->
+  <link rel="stylesheet" href="./styles/home-page.css">
   <script>
     function openPage(link){
       window.open(link,'_self');

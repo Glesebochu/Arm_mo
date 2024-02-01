b31fea565ce18e1fdf104c8d20058985e0deeb39
Fellasfaw <fellasfaw@outlook.com>
2024-02-02 01:38:39 +0300

    Added `Antidote`, `Skills` and `MasteryRequirement`.

---
+++ Middle-logic/Models/MasterRequirement.js
@@ -0,0 +1,6 @@
+export class MasteryRequirement{
+    constructor(prompt, correctValue){
+        this.Prompt = prompt;
+        this.Correct_Value = correctValue;
+    }
+}
---
+++ Middle-logic/Models/Skill.js
@@ -0,0 +1,5 @@
+export class Skill{
+    constructor(description){
+        this.Description = description;
+    }
+}

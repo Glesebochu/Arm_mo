3622e0baef9454214398d5dac43a9a76150143a3
Fellasfaw <fellasfaw@outlook.com>
2024-02-02 09:41:19 +0300

    Editted the `Session` class attributes.

--- Middle-logic/Models/Session.js
+++ Middle-logic/Models/Session.js
@@ -1,11 +1,13 @@
 export class Session {
-    constructor(meditator, activity, startDateTime, endDateTime, stageNumbers = [], ahaMoments = [], steps = []) {
+    constructor(meditator, activity, startDateTime, endDateTime, stageNumbers = [], endingStageNo, ahaMoments = [], steps = [], masteredStage) {
         this.Meditator = meditator;
         this.Activity = activity;
         this.Start_Date_Time = startDateTime;
         this.End_Date_Time = endDateTime;
         this.Stage_Numbers = stageNumbers;
+        this.Ending_Stage_No = endingStageNo;
         this.Aha_Moments = ahaMoments;
         this.Steps = steps;
+        this.Mastered_Stage = masteredStage;
     }
 }

dcd94228b9460490fcfbe6ea4fbe56ffcb14175e
Fellasfaw <fellasfaw@outlook.com>
2024-02-06 11:57:14 +0300

    Added dummy objects for `Activity` class.

--- Middle-logic/Models/Activity.js
+++ Middle-logic/Models/Activity.js
@@ -4,4 +4,23 @@ export class Activity{
         this.Title = title;
         this.Meditation_Object = meditationObject;
     }
+
+    static Breathing = new Activity(
+        1, 
+        "Breathing",
+        "Breath sensations at the nose."
+    )
+    static Reading = new Activity(
+        2,
+        "Reading",
+        "The current sentence."
+    )
+    static Talking = new Activity(
+        3,
+        "Talking",
+        "The current sentence."
+    )
+    static getAllActivities(){
+        return [Activity.Breathing, Activity.Reading, Activity.Talking];
+    }
 }

e972cd7ccb68545d29347629e55c1a50e7f9c519
Fellasfaw <fellasfaw@outlook.com>
2024-02-06 19:54:38 +0300

    Made `Newly_Mastered_Stages` an array on the constructor.

--- Middle-logic/Models/Session.js
+++ Middle-logic/Models/Session.js
@@ -1,12 +1,12 @@
 export class Session {
-    constructor(meditator, startDateTime, endDateTime, practicedStages = [], ahaMoments = [], steps = [], newlyMasteredStages) {
+    constructor(meditator, startDateTime, endDateTime, practicedStages = [], ahaMoments = [], steps = [], newlyMasteredStages = []) {
         this.Meditator = meditator;
         this.Start_Date_Time = startDateTime;
         this.End_Date_Time = endDateTime;
         this.Practiced_Stages = practicedStages;
         this.Aha_Moments = ahaMoments;
-        this.Steps = steps;
         this.Newly_Mastered_Stages = newlyMasteredStages;
+        this.Steps = steps;
     }
 
 }

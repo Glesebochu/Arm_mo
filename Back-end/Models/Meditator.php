c50e2e25122bbb7fcfbb99eba23e5c5a8549abab
Fellasfaw <fellasfaw@outlook.com>
2024-02-07 19:05:51 +0300

    Renamed `ID` to `Meditator_ID` on PHP class.

--- Back-end/Models/Meditator.php
+++ Back-end/Models/Meditator.php
@@ -2,7 +2,7 @@
 // meditator.php
 
 class Meditator {
-    public $ID;
+    public $Meditator_ID;
     public $First_Name;
     public $Last_Name;
     public $Username;
@@ -16,13 +16,13 @@ class Meditator {
         $db->query('USE Arm_mo_v2');
         
         // Query the database based on the identifier
-        $query = "SELECT First_Name, Last_Name, Username, Password, Stage_ID FROM Meditator WHERE Username = '$identifier'";
+        $query = "SELECT * FROM Meditator WHERE Username = '$identifier'";
         $result = $db->query($query);
         $row = $result->fetch_assoc();
         
         // Create a new Meditator object and assign values from the query result
         $meditator = new Meditator();
-        $meditator->ID = $row['ID'];
+        $meditator->Meditator_ID = $row['Meditator_ID'];
         $meditator->First_Name = $row['First_Name'];
         $meditator->Last_Name = $row['Last_Name'];
         $meditator->Username = $row['Username'];

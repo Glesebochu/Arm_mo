656e924b6d8c12f06c9fbe518779ccf839ff0198
Fellasfaw <fellasfaw@outlook.com>
2024-02-08 13:56:12 +0300

    Resolved circular dependency by replacing `import` with `await import()`.

--- Middle-logic/Models/ObservableObject.js
+++ Middle-logic/Models/ObservableObject.js
@@ -1,5 +1,5 @@
-import { SensoryStimulus } from "./SensoryStimulus.js";
-import { MentalObject } from "./MentalObject.js";
+// import { SensoryStimulus } from "./SensoryStimulus.js";
+// import { MentalObject } from "./MentalObject.js";
 
 export class ObservableObjectType{
     static SensoryStimuls = "Sensory Stimulus";
@@ -19,19 +19,21 @@ export class ObservableObject{
         this.Icon = icon;
         this.Intensity = intensity;
     }
-    static getObservableObjectFromObject(obj){
+    static async getObservableObjectFromObject(obj){
         var observableObject;
         if (obj.Type == ObservableObjectType.MentalObject) {
+            const {MentalObject} = await import('./MentalObject.js');
             observableObject = new MentalObject();
             observableObject.Mental_Object_Type = obj.Mental_Object_Type;
         } else {
+            const {SensoryStimulus} = await import('./SensoryStimulus.js');
             observableObject = new SensoryStimulus();
             observableObject.Sensory_Stimulus_Type = obj.Sensory_Stimulus_Type;
         }
 
         observableObject.Observable_Object_ID = obj.Observable_Object_ID;
         observableObject.Title = obj.Title;
-        observableObject.Discriminator = obj.Type;
+        observableObject.Discriminator = obj.Discriminator;
         observableObject.Description = obj.Description;
         observableObject.Icon = obj.Icon;
         observableObject.Intensity = obj.Intensity;

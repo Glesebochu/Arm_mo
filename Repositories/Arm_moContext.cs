91821a99a4343edc0a9611dbe1006561f6efebe4
Fellasfaw <fellasfaw@outlook.com>
2024-04-05 07:20:05 +0300

    Added `Arm_moContext.cs`.

---
+++ Repositories/Arm_moContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Arm_mo.Models;
+
+namespace Arm_mo.Data
+{
+    public class Arm_moContext : DbContext
+    {
+        public DbSet<Meditator> Meditators { get; set; }
+        public DbSet<Stage> Stages { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.;Database=Arm_moDB;  
+                      Trusted_Connection=True; TrustServerCertificate=True");
+        }
+    }
+}

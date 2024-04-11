6d9e973369b955f2154cd2292e4a1a8f23151712
FinhasYohannesGustavo <pj.gustavojhon@gmail.com>
2024-04-11 14:19:41 +0300

    Added the step and activity model classes

---
+++ Models/Activity.cs
@@ -0,0 +1,10 @@
+﻿namespace Arm_mo.Models
+{
+    public class Activity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public MentalObject MentalObject { get; set; }
+
+    }
+}
---
+++ Models/Step.cs
@@ -0,0 +1,24 @@
+﻿namespace Arm_mo.Models
+{
+    public class Step
+    {
+        public enum StepType
+        {
+            Instruction,
+            Question
+        }
+        public enum StepCategory
+        {
+            Prepartaion,
+            Transition
+        }
+        public int ID { get; set; }
+        public StepType Type { get; set; }
+        public StepCategory Category { get; set; }
+        public double Duration { get; set; }
+        public String Response { get; set; }
+        public Activity ? Activity { get; set; }
+
+    }
+
+}

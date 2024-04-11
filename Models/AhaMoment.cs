8fffd035ba9b0635778447eff53971dc508a3716
FinhasYohannesGustavo <pj.gustavojhon@gmail.com>
2024-04-11 10:30:50 +0300

    Added the Stage and AhaMoment Model classes, with thier attributes. Session is still incomplete due to not adding Step and and ObservableObject Model classes.

---
+++ Models/AhaMoment.cs
@@ -0,0 +1,9 @@
+﻿namespace Arm_mo.Models
+{
+    public class AhaMoment
+    {
+        public int Id { get; set; }
+        public Session Session { get; set; }
+        public String Label { get; set; }
+    }
+}
---
+++ Models/Session.cs
@@ -0,0 +1,13 @@
+﻿namespace Arm_mo.Models
+{
+    public class Session
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public Meditator Meditator { get; set; }
+        public List<AhaMoment> ahaMoments { get; set; }
+        public List<Stage> pracitcedStages { get; set; }
+
+    }
+}

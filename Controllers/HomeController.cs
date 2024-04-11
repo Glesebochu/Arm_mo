afc97e1b9423e1c97ef97504fc92fbfcd8bde712
FinhasYohannesGustavo <pj.gustavojhon@gmail.com>
2024-04-11 15:23:33 +0300

    Fixed an issue where there was ambiguity between the built in Activity class and the model class Activity

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,7 +26,7 @@ namespace Arm_mo.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel { RequestId = System.Diagnostics.Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }

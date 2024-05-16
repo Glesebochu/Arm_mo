b4a077d43da452d67dc7481489b40bf20593a18a
Abenezer Walelign <walelignabenezer@gmail.com>
2024-05-16 09:09:45 +0300

    Removed unnecessary imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './output.css'
 import {Button} from './components/ui/button'
-import {Checkbox} from './components/ui/checkbox'
+import {Input} from './components/ui/input'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1 className='text-red'>Hello Test</h1>
-      <Button>Yelow</Button>
-      <Checkbox />
+
     </>
   )
 }

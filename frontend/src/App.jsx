83bbe47bba3e1c95a6b43218a519ced30dd6c01e
Naod816 <naodteen816@gmail.com>
2024-05-21 17:16:52 +0300

    Added Settings Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import Signin from "./Pages/Signin"
 import Signup from "./Pages/Signup"
+import Setting from "./Pages/Settings"
+
 function App() {
   return (
     <>
      <Signup/>
      <Signin/>
+     <Setting/>
     </>
   )
 }

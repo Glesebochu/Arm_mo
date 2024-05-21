16280ec75bccbc9eeca137349390a193020f4f08
FinhasYohannesGustavo <pj.gustavojhon@gmail.com>
2024-05-21 09:36:39 +0300

    Removed the IsMastered Field from stage and removed some of the items that should not be mapped.

--- backend/Models/Stage.cs
+++ backend/Models/Stage.cs
@@ -1,14 +1,22 @@
-﻿namespace Arm_mo.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+namespace Arm_mo.Models
 {
     public class Stage
     {
         public int Id { get; set; }
         public string Goal { get; set; }
+
+        [NotMapped]       
         public List<string> Intentions { get; set; }
+
+        [NotMapped]
         public List<string> Obstacles { get; set; }
+
+        [NotMapped]
         public List<string> Skills { get; set; }
+        
+        [NotMapped]
         public List<string> MasteryRequirements { get; set; }
-        public bool IsMastered { get; set; }
         public Stage()
         {
 

f41cd9483ec8e83f78654fa43877e46ab5a53431
FinhasYohannesGustavo <pj.gustavojhon@gmail.com>
2024-05-23 10:40:58 +0300

    Added the Navigation Bar so that Meditators can switch from App Activity to Session Summary when choosing the analytics that they will see.

--- frontend/src/Pages/SessionSummary.jsx
+++ frontend/src/Pages/SessionSummary.jsx
@@ -1,14 +1,28 @@
-import UsageView from "../components/ui/Usage.jsx";
-import {DataTableDemo} from "../components/Custom/SessionTable.jsx";
-const SessionSummary =()=>{
-    return (
-        <div>
-            <h1  className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl"> h1</h1>
-            <UsageView/>
-            <DataTableDemo/>
-                        
-        </div>
-    );
-}
+import * as React from "react";
+import { useState } from "react";
+import UsageView from "@/components/ui/Usage.jsx";
+import { DataTableDemo } from "@/components/Custom/SessionTable.jsx";
+import { NavigationMenuDemo } from "@/components/Custom/AnalyticsNavigation.jsx";
+
+const SessionSummary = () => {
+  const [selectedView, setSelectedView] = useState("DataTable");
+
+  const renderSelectedView = () => {
+    switch (selectedView) {
+      case "UsageView":
+        return <UsageView />;
+      case "DataTable":
+      default:
+        return <DataTableDemo />;
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center  space-y-8">
+      <NavigationMenuDemo setSelectedView={setSelectedView} />
+      {renderSelectedView()}
+    </div>
+  );
+};
 
 export default SessionSummary;

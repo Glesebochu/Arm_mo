123cd792eef391dbb2df8e68f9fb037d6836feb9
Abenezer Walelign <walelignabenezer@gmail.com>
2024-05-26 11:19:45 +0300

    Added the SignUp component for testing registration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import * as React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SessionSummary from "./Pages/AnalyticsPage";
 import SessionDetails from "./Pages/SessionDetails"; // Import the new component for session details
+import Signup from "./Pages/Signup";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/signup" element={<Signup/>}/>
         <Route path="/" element={<SessionSummary />} />
         <Route path="/session/:sessionId" element={<SessionDetails />} />
       </Routes>

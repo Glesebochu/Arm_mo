98f4501328f75098136ff9898dc80880c71d42b0
FinhasYohannesGustavo <pj.gustavojhon@gmail.com>
2024-05-28 09:15:40 +0300

    Added a comment to specify what to change after creating a pull request to this branch in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,10 @@ function App() {
   return (
     <Router>
       <Routes>
+        {/* For your specific Modules Modify this so that your Components show up when you run Vite */}
         <Route path="/" element={<SessionSummary />} />
+        
+        {/* No need to to remove the below */}
         <Route path="/session/:sessionId" element={<SessionDetails />} />
         <Route path="/Usage" element={<UsageView />} />
         <Route path="/Insights" element={<Insights />} />

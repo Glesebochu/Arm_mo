a2ee6fa38e5c74cbb632a29e14c1be6372ac13f0
FinhasYohannesGustavo <pj.gustavojhon@gmail.com>
2024-06-01 17:36:00 +0300

    Added Migrations to add IsDeleted property on session table.

---
+++ backend/Migrations/20240601114923_AddedIsDeletedPropertyToSession.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedIsDeletedPropertyToSession : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Sessions",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Sessions");
+        }
+    }
+}

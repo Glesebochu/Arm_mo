9f204f6a682cd06837a2628587bc11cd7b081ee2
FinhasYohannesGustavo <pj.gustavojhon@gmail.com>
2024-06-03 13:07:24 +0300

    Changed the date time fields to be of start datetime and end date time

--- backend/Models/Session.cs
+++ backend/Models/Session.cs
@@ -5,8 +5,8 @@ namespace backend.Models
     public class Session
     {
         public int Id { get; set; }
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
        
         [Column("MeditatorID")]
         public Meditator Meditator { get; set; }
@@ -15,17 +15,17 @@ namespace backend.Models
         public List<NewlyMasteredStage> NewlyMasterdStages {  get; set; }
         public List<ObservableObject> ObservableObjects { get; set; }
         public PreparationPhase PreparationPhase { get; set; }
-        public bool IsDeleted { get; set; } = false;
         public int PreparationPhaseId { get; set; } // Foreign key property
+        public bool IsDeleted { get; set; } = false;
         public Session()
         {
 
         }
-        public Session(DateTime startTime,DateTime endTime, Meditator meditator,List<AhaMoment> ahaMoments,
+        public Session(DateTime startDateTime,DateTime endDateTime, Meditator meditator,List<AhaMoment> ahaMoments,
             List<PracticedStage> practicedStages,List<NewlyMasteredStage> newlyMasteredStages,List<ObservableObject> observableObjects)
         {
-            StartTime = startTime;
-            EndTime = endTime;
+            StartDateTime = startDateTime;
+            EndDateTime = endDateTime;
             Meditator = meditator;
             AhaMoments = ahaMoments;
             PracticedStages = practicedStages;

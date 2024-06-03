a4ba1d7aac8b12c59bc45efda25850d53da03764
FinhasYohannesGustavo <pj.gustavojhon@gmail.com>
2024-06-03 12:45:30 +0300

    Added String.Empty to string fields

--- backend/Models/Address.cs
+++ backend/Models/Address.cs
@@ -2,9 +2,9 @@ namespace backend.Models{
     public class Address
     {
         public int Id { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string Country { get; set; }
+        public string City { get; set; } = String.Empty;
+        public string State { get; set; } = String.Empty;
+        public string Country { get; set; } = String.Empty;
 
     }
 }

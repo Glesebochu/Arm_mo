077575a4266d6aa7b463ae2a29ffe569db5e0087
FinhasYohannesGustavo <pj.gustavojhon@gmail.com>
2024-06-03 12:47:53 +0300

    Made Goal dates to be DateOnlys

--- backend/Models/Goal.cs
+++ backend/Models/Goal.cs
@@ -7,24 +7,16 @@ namespace backend.Models
         InProgress,
         Completed
     }
-
     public class Goal
     {
         public int Id { get; set; }
         public GoalStatus Status { get; set; }
-        public Activity Activity { get; set; }
-        public ObservableObject? MeditationObject { get; set; }
-        public DateTime DueDateTime { get; set; }
-        public DateTime CompletedDateTime { get; set; }
         public Goal? ParentGoal { get; set; }
         public List<Goal>? ChildGoals { get; set; }
-
+        public DateOnly DueDate { get; set; }
+        public DateOnly CompletedDate { get; set; }
+        public Activity Activity { get; set; }
+        public ObservableObject? MeditationObject { get; set; }
         public Goal() { }
-        public Goal(Activity activity, ObservableObject meditationObject)
-        {
-            this.Activity = activity;
-            this.MeditationObject = meditationObject;
-        }
-
     }
 }

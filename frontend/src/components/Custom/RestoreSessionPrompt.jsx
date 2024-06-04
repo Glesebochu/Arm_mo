b6c677f3265b283e7d061163c6dfb5b7e0e12917
FinhasYohannesGustavo <pj.gustavojhon@gmail.com>
2024-06-04 15:57:50 +0300

    Fixed styling issues when trying to delete and restore sessions

--- frontend/src/components/Custom/RestoreSessionPrompt.jsx
+++ frontend/src/components/Custom/RestoreSessionPrompt.jsx
@@ -1,8 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-function RestoreSessionPrompt({ sessionId, onDelete, onCancel }) {
+function RestoreSessionPrompt({ sessionId, onCancel }) {
   const [isRestoring, setisRestoring] = useState(false);
+
+  useEffect(() => {
+    document.body.classList.add("overflow-hidden");
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, []);
 
   const handleRestore = async (id) => {
     setisRestoring(true);
@@ -10,9 +17,7 @@ function RestoreSessionPrompt({ sessionId, onDelete, onCancel }) {
       await axios.post(
         `http://localhost:5158/api/Analyzer/RestoreSession?SessionId=${id}`
       );
-      setTimeout(() => {
-        window.location.reload();
-      }, 1000); // Delay to allow animation to play
+      window.location.reload();
     } catch (error) {
       console.error("There was an error restoring the session!", error);
       setisRestoring(false);
@@ -20,12 +25,10 @@ function RestoreSessionPrompt({ sessionId, onDelete, onCancel }) {
   };
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75">
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 z-50">
       <div
-        className={`bg-white p-6 rounded-lg shadow-lg transition-transform duration-500 ${
-          isRestoring
-            ? "transform scale-0 opacity-0"
-            : "transform scale-100 opacity-100"
+        className={`bg-white p-6 rounded-lg shadow-lg transition-transform duration-500 transform ${
+          isRestoring ? "scale-0 opacity-0" : "scale-100 opacity-100"
         }`}
       >
         <h2 className="text-xl font-bold mb-4">Confirm Restore</h2>
@@ -33,13 +36,13 @@ function RestoreSessionPrompt({ sessionId, onDelete, onCancel }) {
         <div className="flex justify-end">
           <button
             onClick={onCancel}
-            className="bg-gray-300 text-gray-700 px-4 py-2 rounded mr-2"
+            className="bg-gray-300 text-gray-700 px-4 py-2 rounded mr-2 hover:bg-grey-600"
           >
             Cancel
           </button>
           <button
             onClick={() => handleRestore(sessionId)}
-            className="bg-blue-500 text-white px-4 py-2 rounded"
+            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500"
           >
             Restore
           </button>

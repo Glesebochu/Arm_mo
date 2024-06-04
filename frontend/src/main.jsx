fbb2173e42baabfc89e73702737b06b9e36b871f
Naod816 <naodteen816@gmail.com>
2024-06-04 17:17:16 +0300

    Wrapped the app with GoogleOAuthProvider and imported the neccesary things for it to work fine

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GoogleOAuthProvider clientId="913944271119-u2lk2j45nbghfavlffabh46eqnf35umd.apps.googleusercontent.com">
     <App />
+    </GoogleOAuthProvider>;
   </React.StrictMode>,
 )

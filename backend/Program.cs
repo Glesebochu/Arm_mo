1660aed66723c9a5590a191230de0fa097addde1
Abenezer Walelign <walelignabenezer@gmail.com>
2024-06-05 18:29:27 +0300

    Added dotenv for working with .env files

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,9 +1,11 @@
+using dotenv.net;
 using Arm_mo.Context;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using System.Text.Json.Serialization;
+using AutoMapper.Internal.Mappers;
 
 namespace backend
 {
@@ -11,6 +13,9 @@ namespace backend
     {
         public static void Main(string[] args)
         {
+            // Load environment variables from .env file
+            DotEnv.Load();
+
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddCors(options =>
             {
@@ -19,14 +24,46 @@ namespace backend
                         .AllowAnyMethod()
                         .AllowCredentials()
                         .SetIsOriginAllowed((host) => { return host == "http://localhost:5173"; })
-                        .SetIsOriginAllowed((host) => { return host == "http://localhost:5173"; })
                         .AllowAnyHeader());
             });
 
+            // builder.Services.AddCors(options =>
+            // {
+            //     options.AddPolicy("AllowedSpecificOrigins",
+            //         builder => builder
+            //             .WithOrigins("http://localhost:5173")
+            //             .AllowAnyMethod()
+            //             .AllowCredentials()
+            //             .AllowAnyHeader());
+            // });
+
+            builder.Services.AddControllers()
+            .ConfigureApiBehaviorOptions(options =>
+            {
+                options.InvalidModelStateResponseFactory = CustomModelStateResponseFactory.InvalidModelStateResponse;
+            });
+            
             // Configure JWT authentication
             var jwtKey = Environment.GetEnvironmentVariable("JWT_KEY");
             var jwtIssuer = Environment.GetEnvironmentVariable("JWT_ISSUER");
             var jwtAudience = Environment.GetEnvironmentVariable("JWT_AUDIENCE");
+
+            if (string.IsNullOrEmpty(jwtKey) || string.IsNullOrEmpty(jwtIssuer) || string.IsNullOrEmpty(jwtAudience))
+            {
+                Console.WriteLine($"JWT_KEY: {jwtKey}");
+                Console.WriteLine($"JWT_ISSUER: {jwtIssuer}");
+                Console.WriteLine($"JWT_AUDIENCE: {jwtAudience}");
+                throw new ArgumentNullException("One of the JWT environment variables is null or empty");
+            }
+
+            // Decode the base64 key and check its length
+            var keyBytes = Convert.FromBase64String(jwtKey);
+            Console.WriteLine($"Key Length: {keyBytes.Length * 8} bits");
+
+            if (keyBytes.Length < 16) // 128 bits
+            {
+                throw new ArgumentException("The key is too short. It must be at least 128 bits.");
+            }
 
             builder.Services.AddAuthentication(options =>
             {
@@ -35,6 +72,16 @@ namespace backend
             })
             .AddJwtBearer(options =>
             {
+                options.Events = new JwtBearerEvents(){
+                 OnMessageReceived = context => {
+                    var token = context.Request.Cookies["token"];
+                    if(!string.IsNullOrEmpty(token)){
+                        context.Token = token;
+                    }
+                    
+                    return Task.CompletedTask;
+                 }   
+                };
                 options.TokenValidationParameters = new TokenValidationParameters
                 {
                     ValidateIssuer = true,
@@ -43,12 +90,11 @@ namespace backend
                     ValidateIssuerSigningKey = true,
                     ValidIssuer = jwtIssuer,
                     ValidAudience = jwtAudience,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+                    IssuerSigningKey = new SymmetricSecurityKey(keyBytes)
                 };
             });
 
             builder.Services.AddScoped<IUserService, UserService>();
-            //Adding the Arm'mo context
             builder.Services.AddDbContextPool<Arm_moContext>(option => option.
             UseSqlServer(builder.Configuration.GetConnectionString("Arm_moDbConnection")));
             builder.Services.AddControllers().AddJsonOptions(options =>
@@ -73,7 +119,6 @@ namespace backend
             app.UseAuthorization();
             app.MapControllers();
             app.Run();
-
         }
     }
 }

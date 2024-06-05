e46db63e6ad34ad9c9bbe4db2339f20ec5459495
Abenezer Walelign <walelignabenezer@gmail.com>
2024-06-05 18:37:21 +0300

    Imported Provider for working with redux store, imported the store itself and the ToastContainer for working with react-toastify

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Provider } from "react-redux";
+import { store } from '../store/store.js';
+import { ToastContainer } from 'react-toastify';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+      <App />
+  </Provider>
 )

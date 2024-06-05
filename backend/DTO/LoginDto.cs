f04cfa41371a594db61b2b42da891fa8ff8049cb
Abenezer Walelign <walelignabenezer@gmail.com>
2024-06-05 18:26:00 +0300

    Created LoginDto

--- backend/DTO/LoginDto.cs
+++ backend/DTO/LoginDto.cs
@@ -3,7 +3,8 @@ using System.ComponentModel.DataAnnotations;
 public class LoginDto
 {
     [Required]
-    public string Username { get; set; }
+    [EmailAddress]
+    public string Email { get; set; }
 
     [Required]
     public string Password { get; set; }

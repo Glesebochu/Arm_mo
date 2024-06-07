3c049ea934f3071d24ecf4a1fa521e7ef53e34fd
Abenezer Walelign <walelignabenezer@gmail.com>
2024-06-07 19:27:53 +0300

    Made routes Protected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,39 @@
 import * as React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import SessionDetails from "./pages/SessionDetails"; // Import the new component for session details
+import SessionDetails from "./pages/SessionDetails";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import UsageView from "./pages/Usage";
 import Insights from "./pages/Insights";
 import Landing from "./pages/Landing";
 import ProtectedRoute from "../utils/ProtectedRoute";
-import { useEffect } from "react";
-import { fetchMe } from "../Slices/AuthSlice";
-import { useDispatch } from "react-redux";
 
 function App() {
-  
   return (
     <Router>
       <Routes>
         <Route path="/signup" element={<Signup/>}/>
         <Route path="/signin" element={<Signin/>}/>
+        <Route path="/" element={
+          <Landing />
+        } 
+        />
         <Route path="/Usage" element={
           <ProtectedRoute>
             <UsageView />
           </ProtectedRoute>
         } 
         />
-        <Route path="/session/:sessionId" element={<SessionDetails />} />
-        <Route path="/Insights" element={<Insights />} />
+        <Route path="/session/:sessionId" element={
+          <ProtectedRoute>
+            <SessionDetails/>
+          </ProtectedRoute>
+        } />
+        <Route path="/Insights" element={
+          <ProtectedRoute>
+            <Insights/>
+          </ProtectedRoute>
+        } />
       </Routes>
     </Router>
   );

6a9bcd9dc66a98451120e3bb5e6f701cfb07bcf6
Abenezer Walelign <walelignabenezer@gmail.com>
2024-06-08 12:39:49 +0300

    Implemented functionality for the Navbar's Login button.

--- frontend/src/components/Custom/NavBar.jsx
+++ frontend/src/components/Custom/NavBar.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const NavBar = () => {
     const [isScrolled, setIsScrolled] = useState(false);
+    const navigate = useNavigate();
 
     useEffect(() => {
         const handleScroll = () => {
@@ -16,6 +18,10 @@ const NavBar = () => {
         };
     }, []);
 
+    const navigateToSigninScreen = ()=> {
+        navigate("/Signin")
+    }
+
     return (
         <div className={`bg-transparent sticky top-0 z-50 ${isScrolled ? 'backdrop-blur' : ''}`}>
             <div className="bg-transparent flex justify-between items-center backdrop-bl px-4 py-4 max-w-[1400px] m-auto">
@@ -23,7 +29,7 @@ const NavBar = () => {
                     Arm&rsquo;mo
                 </h1>
                 <div className="flex">
-                    <button className="font-k2d font-bold flex items-center px-8 py-1.5  border-2 border-black dark:border-white uppercase bg-white text-black transition duration-200 text-sm shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] ">
+                    <button onClick={navigateToSigninScreen} className="font-k2d font-bold flex items-center px-8 py-1.5  border-2 border-black dark:border-white uppercase bg-white text-black transition duration-200 text-sm shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] ">
                         Login
                     </button>
                     {/* <Button className="font-delius font-bold px-8 text-[15px]">

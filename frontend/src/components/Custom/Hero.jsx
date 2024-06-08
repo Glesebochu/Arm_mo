47deaa445468e414fabf92b27463795c4232ee3d
Abenezer Walelign <walelignabenezer@gmail.com>
2024-06-08 12:40:38 +0300

    Implemented functionality for the Landing pages GetStarted button.

--- frontend/src/components/Custom/Hero.jsx
+++ frontend/src/components/Custom/Hero.jsx
@@ -5,9 +5,16 @@ import Arc from "./Arc";
 import UnderLine from "./Underline";
 import React from 'react'
 import { SparklesCore } from "./Sparkles";
+import { useNavigate } from "react-router-dom";
 
 const words = ["Inner Peace", "Focus", "Zen"];
 const Hero = () => {
+  const navigate = useNavigate();
+
+  const navigateToSignupScreen = ()=> {
+    navigate("/Signup")
+  }
+
   return (
     <div className="flex items-center justify-between w-full max-w-[1400px] m-auto mt-10 flex-col lg:flex-row">
       {/* Left Hero pannel */}
@@ -65,7 +72,7 @@ const Hero = () => {
           </div>
         </div>
         <div className="mt-20 lg:mt-12 lg:ml-[-195px] ">
-          <button className="flex items-center px-8 py-1.5  border-2 border-black dark:border-white uppercase bg-white text-black transition duration-200 text-sm shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] ">
+          <button onClick={navigateToSignupScreen} className="flex items-center px-8 py-1.5  border-2 border-black dark:border-white uppercase bg-white text-black transition duration-200 text-sm shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] ">
             <span className="font-k2d font-bold">Get Started</span> <FaArrowRight className="m-2"/>
           </button>
         </div>

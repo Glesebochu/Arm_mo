85d02aba2b728e36febead8fb241ae9582e570f9
Abenezer Walelign <walelignabenezer@gmail.com>
2024-06-09 15:39:08 +0300

    Made LastName nullable

--- backend/Migrations/20240608161326_MadeLastNameNullable.Designer.cs
+++ backend/Migrations/20240608161326_MadeLastNameNullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using backend.Data;
 
@@ -11,9 +12,11 @@ using backend.Data;
 namespace backend.Migrations
 {
     [DbContext(typeof(Arm_moContext))]
-    partial class Arm_moContextModelSnapshot : ModelSnapshot
+    [Migration("20240608161326_MadeLastNameNullable")]
+    partial class MadeLastNameNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

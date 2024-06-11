a9aa9a7416684e1f1524b9456eb9d58933b04e6f
Fellasfaw <fellasfaw@outlook.com>
2024-06-11 08:50:54 +0300

    Fixed import for `preparationPhaseReducer` in `store.js`

--- frontend/src/Pages/PreparationPhases/CreatePreparationPhase.jsx
+++ frontend/src/Pages/PreparationPhases/CreatePreparationPhase.jsx
@@ -27,160 +27,160 @@ export default function CreatePreparationPhase() {
     EndDateTime: "",
   });
 
-  // const steps = [
-  //   {
-  //     title: "Goal",
-  //     instruction:
-  //       "Set a complete and achievable goal for your meditation session",
-  //     component: (
-  //       <Input
-  //         value={preparationData.Goals}
-  //         onChange={(e) =>
-  //           setPreparationData({ ...preparationData, Goals: e.target.value })
-  //         }
-  //       />
-  //     ),
-  //     buttons: ["Next step", "Cancel"],
-  //   },
-  //   {
-  //     title: "Duration",
-  //     instruction: "Set a reasonable timer for your meditation session",
-  //     component: (
-  //       <TimeInput
-  //         defaultValue={new Time(1, 0)}
-  //         onChange={(e) =>
-  //           setPreparationData({ ...preparationData, Duration: e.target.value })
-  //         }
-  //       />
-  //     ),
-  //     buttons: ["Previous", "Next step", "Cancel"],
-  //   },
-  //   {
-  //     title: "Motivation",
-  //     instruction:
-  //       "Write a complete and meaningful statement about what motivates you to meditate.",
-  //     component: (
-  //       <Input
-  //         value={preparationData.Motivation}
-  //         onChange={(e) =>
-  //           setPreparationData({
-  //             ...preparationData,
-  //             Motivation: e.target.value,
-  //           })
-  //         }
-  //       />
-  //     ),
-  //     buttons: ["Previous", "Next step", "Cancel"],
-  //   },
-  //   {
-  //     title: "Distraction",
-  //     instruction:
-  //       "Properly articulate any potential distractions that may affect your meditation.",
-  //     component: (
-  //       <Input
-  //         value={preparationData.Distractions}
-  //         onChange={(e) =>
-  //           setPreparationData({
-  //             ...preparationData,
-  //             Distractions: e.target.value.split(","),
-  //           })
-  //         }
-  //       />
-  //     ),
-  //     buttons: ["Previous", "Next step", "Cancel"],
-  //   },
-  //   {
-  //     title: "Expectation",
-  //     instruction:
-  //       "Write a complete and meaningful statement about your expectations for this session.",
-  //     component: (
-  //       <Input
-  //         value={preparationData.Expectation}
-  //         onChange={(e) =>
-  //           setPreparationData({
-  //             ...preparationData,
-  //             Expectation: e.target.value,
-  //           })
-  //         }
-  //       />
-  //     ),
-  //     buttons: ["Previous", "Next step", "Cancel"],
-  //   },
-  //   {
-  //     title: "Diligence",
-  //     instruction:
-  //       "Intend to focus diligently during your meditation. Click 'Ready' to proceed.",
-  //     component: <Input />, // Placeholder for any additional components
-  //     buttons: ["Previous", "Cancel", "Ready"],
-  //   },
-  //   {
-  //     title: "Posture",
-  //     instruction:
-  //       "Correct your posture according to the user guide. Click 'Save' to save all your data & move on to the transition phase.",
-  //     component: null, // No additional components
-  //     buttons: ["Previous", "Cancel", "Save"],
-  //   },
-  // ];
+  const steps = [
+    {
+      title: "Goal",
+      instruction:
+        "Set a complete and achievable goal for your meditation session",
+      component: (
+        <Input
+          value={preparationData.Goals}
+          onChange={(e) =>
+            setPreparationData({ ...preparationData, Goals: e.target.value })
+          }
+        />
+      ),
+      buttons: ["Next step", "Cancel"],
+    },
+    {
+      title: "Duration",
+      instruction: "Set a reasonable timer for your meditation session",
+      component: (
+        <TimeInput
+          defaultValue={new Time(1, 0)}
+          onChange={(e) =>
+            setPreparationData({ ...preparationData, Duration: e.target.value })
+          }
+        />
+      ),
+      buttons: ["Previous", "Next step", "Cancel"],
+    },
+    {
+      title: "Motivation",
+      instruction:
+        "Write a complete and meaningful statement about what motivates you to meditate.",
+      component: (
+        <Input
+          value={preparationData.Motivation}
+          onChange={(e) =>
+            setPreparationData({
+              ...preparationData,
+              Motivation: e.target.value,
+            })
+          }
+        />
+      ),
+      buttons: ["Previous", "Next step", "Cancel"],
+    },
+    {
+      title: "Distraction",
+      instruction:
+        "Properly articulate any potential distractions that may affect your meditation.",
+      component: (
+        <Input
+          value={preparationData.Distractions}
+          onChange={(e) =>
+            setPreparationData({
+              ...preparationData,
+              Distractions: e.target.value.split(","),
+            })
+          }
+        />
+      ),
+      buttons: ["Previous", "Next step", "Cancel"],
+    },
+    {
+      title: "Expectation",
+      instruction:
+        "Write a complete and meaningful statement about your expectations for this session.",
+      component: (
+        <Input
+          value={preparationData.Expectation}
+          onChange={(e) =>
+            setPreparationData({
+              ...preparationData,
+              Expectation: e.target.value,
+            })
+          }
+        />
+      ),
+      buttons: ["Previous", "Next step", "Cancel"],
+    },
+    {
+      title: "Diligence",
+      instruction:
+        "Intend to focus diligently during your meditation. Click 'Ready' to proceed.",
+      component: <Input />, // Placeholder for any additional components
+      buttons: ["Previous", "Cancel", "Ready"],
+    },
+    {
+      title: "Posture",
+      instruction:
+        "Correct your posture according to the user guide. Click 'Save' to save all your data & move on to the transition phase.",
+      component: null, // No additional components
+      buttons: ["Previous", "Cancel", "Save"],
+    },
+  ];
 
-  // const handleNext = () => {
-  //   setStepIndex((prevIndex) => prevIndex + 1);
-  // };
+  const handleNext = () => {
+    setStepIndex((prevIndex) => prevIndex + 1);
+  };
 
-  // const handlePrevious = () => {
-  //   setStepIndex((prevIndex) => prevIndex - 1);
-  // };
+  const handlePrevious = () => {
+    setStepIndex((prevIndex) => prevIndex - 1);
+  };
 
-  // const handleSave = () => {
-  //   const preparationPhaseData = {
-  //     Duration: preparationData.Duration.toString(),
-  //     Motivation: preparationData.Motivation,
-  //     Goals: preparationData.Goals,
-  //     Expectation: preparationData.Expectation,
-  //     Distractions: preparationData.Distractions,
-  //     StartDateTime: preparationData.StartDateTime,
-  //     EndDateTime: new Date().toISOString(),
-  //   };
-  //   dispatch(CreatePreparationPhaseThunk(preparationPhaseData)); // Save preparation phase data
-  // };
+  const handleSave = () => {
+    const preparationPhaseData = {
+      Duration: preparationData.Duration.toString(),
+      Motivation: preparationData.Motivation,
+      Goals: preparationData.Goals,
+      Expectation: preparationData.Expectation,
+      Distractions: preparationData.Distractions,
+      StartDateTime: preparationData.StartDateTime,
+      EndDateTime: new Date().toISOString(),
+    };
+    dispatch(CreatePreparationPhaseThunk(preparationPhaseData)); // Save preparation phase data
+  };
 
-  // const handleCancel = () => {
-  //   setPreparationData({
-  //     Duration: new Time(1, 0),
-  //     Motivation: "",
-  //     Goals: [],
-  //     Expectation: "",
-  //     Distractions: [],
-  //     StartDateTime: "",
-  //     EndDateTime: "",
-  //   });
-  //   setStepIndex(0);
-  // };
+  const handleCancel = () => {
+    setPreparationData({
+      Duration: new Time(1, 0),
+      Motivation: "",
+      Goals: [],
+      Expectation: "",
+      Distractions: [],
+      StartDateTime: "",
+      EndDateTime: "",
+    });
+    setStepIndex(0);
+  };
 
-  // const handleAction = (buttonLabel) => {
-  //   if (buttonLabel === "Next step" || buttonLabel === "Ready") {
-  //     handleNext();
-  //   } else if (buttonLabel === "Save") {
-  //     handleSave();
-  //   } else if (buttonLabel === "Cancel") {
-  //     handleCancel();
-  //   }
-  // };
+  const handleAction = (buttonLabel) => {
+    if (buttonLabel === "Next step" || buttonLabel === "Ready") {
+      handleNext();
+    } else if (buttonLabel === "Save") {
+      handleSave();
+    } else if (buttonLabel === "Cancel") {
+      handleCancel();
+    }
+  };
 
-  // useEffect(() => {
-  //   if (stepIndex === 0) {
-  //     const timer = setTimeout(() => {
-  //       setPreparationData({
-  //         ...preparationData,
-  //         StartDateTime: new Date().toISOString(),
-  //       });
-  //     }, 5000);
-  //     return () => clearTimeout(timer);
-  //   }
-  // }, [stepIndex]);
+  useEffect(() => {
+    if (stepIndex === 0) {
+      const timer = setTimeout(() => {
+        setPreparationData({
+          ...preparationData,
+          StartDateTime: new Date().toISOString(),
+        });
+      }, 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [stepIndex]);
 
-  // const currentStep = steps[stepIndex];
-  // const isFirstStep = stepIndex === 0;
-  // const isLastStep = stepIndex === steps.length - 1;
+  const currentStep = steps[stepIndex];
+  const isFirstStep = stepIndex === 0;
+  const isLastStep = stepIndex === steps.length - 1;
 
   return (
     <div>
@@ -189,7 +189,7 @@ export default function CreatePreparationPhase() {
         Instruction Description → Hey there! Let's take your meditation practice
         to the next level by answering some important questions.
       </p>
-      {/* <Progress value={stepIndex + 1} max={steps.length} />
+      <Progress value={stepIndex + 1} max={steps.length} />
       <h2>Title- {currentStep.title}</h2>
       <p>Instruction- {currentStep.instruction}</p>
       {currentStep.component}
@@ -200,7 +200,7 @@ export default function CreatePreparationPhase() {
             {buttonLabel}
           </Button>
         ))}
-      </div> */}
+      </div>
     </div>
   );
 }

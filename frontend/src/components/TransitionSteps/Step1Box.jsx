36a6836bf4cdc93502e8da4d63a0cdb3c761e746
Fellasfaw <fellasfaw@outlook.com>
2024-06-12 18:38:48 +0300

    Improved `Step1Box`

--- frontend/src/components/TransitionSteps/Step1Box.jsx
+++ frontend/src/components/TransitionSteps/Step1Box.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { Button } from '../ui/button';
 import { Card } from '../ui/card';
 
-export function Step1Box({ onDone }) {
+export function Step1Box({ meditationObjectType, onDone }) {
+
+    const typeText = meditationObjectType == "MentalObject" ? "Mental Objects" : "Sensory Stimuli"
+
     return (
         <Card className="step-box w-full h-full flex flex-col items-center justify-center p-6 bg-white shadow-lg rounded-lg">
 
-
-            <p className="text-2xl text-gray-700">
-                Be open to all observable objects in the present moment and relax.
-            </p>
-
+            <h2 className="text-2xl text-gray-700 text-center m-10">
+                Relax and be open to all <span className='text-cyan-800'>{typeText}</span> in this moment.
+            </h2>
 
             {/* <Button onClick={onDone} className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">
                 Done

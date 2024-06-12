65470458b1120f92d72c224dc3ab94ae265907be
Fellasfaw <fellasfaw@outlook.com>
2024-06-12 10:50:30 +0300

    Improved the grid layout of the `TransitionPhase` box.

--- frontend/src/components/TransitionSteps/Step1Box.jsx
+++ frontend/src/components/TransitionSteps/Step1Box.jsx
@@ -5,12 +5,16 @@ import { Card } from '../ui/card';
 export function Step1Box({ onDone }) {
     return (
         <Card className="step-box w-full h-full flex flex-col items-center justify-center p-6 bg-white shadow-lg rounded-lg">
-            <p className="text-lg text-gray-700 mb-4">
+
+
+            <p className="text-2xl text-gray-700">
                 Be open to all observable objects in the present moment and relax.
             </p>
-            <Button onClick={onDone} className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">
+
+
+            {/* <Button onClick={onDone} className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">
                 Done
-            </Button>
+            </Button> */}
         </Card>
     );
 };

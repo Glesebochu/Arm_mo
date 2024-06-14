9694b44249fbda17af989284c7622faf6090fd28
Abenezer Walelign <walelignabenezer@gmail.com>
2024-06-14 11:39:44 +0300

    Updated IUserService and UserService to include UpdateUserAsync method

--- backend/Services/IUserService.cs
+++ backend/Services/IUserService.cs
@@ -5,4 +5,5 @@ public interface IUserService
     Task<bool> RegisterUserAsync(Meditator user);
     Task<Meditator> AuthenticateAsync(string username, string password);
     Task<Meditator> GetUserByUsernameAsync(string username);
+    Task<ServiceResult<Meditator>> UpdateUserAsync(UpdateUserAccountDTO updateUserAccountDto);
 }
--- backend/Services/UserService.cs
+++ backend/Services/UserService.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 public class UserService : IUserService
 {
     private readonly Arm_moContext _context;
-
+    private readonly string _storagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads");
     public UserService(Arm_moContext context)
     {
         _context = context;
@@ -38,5 +38,59 @@ public class UserService : IUserService
     public async Task<Meditator> GetUserByUsernameAsync(string username)
     {
         return await _context.Meditators.SingleOrDefaultAsync(u => u.Username == username);
+    }
+
+    public async Task<ServiceResult<Meditator>> UpdateUserAsync(UpdateUserAccountDTO updateUserAccountDto)
+    {
+        var user = await _context.Meditators.FindAsync(updateUserAccountDto.Id);
+        if (user == null)
+        {
+            return new ServiceResult<Meditator>
+            {
+                Success = false,
+                Message = "User not found."
+            };
+        }
+
+        if (updateUserAccountDto.ProfilePicture != null)
+        {
+            user.ProfilePicture = await SaveFileAsync(updateUserAccountDto.ProfilePicture);
+        }
+
+        user.FirstName = updateUserAccountDto.FirstName;
+        user.LastName = updateUserAccountDto.LastName;
+        user.Username = updateUserAccountDto.Username;
+
+        _context.Meditators.Update(user);
+        await _context.SaveChangesAsync();
+        
+        user = _context.Meditators.Where(u => u.Id == updateUserAccountDto.Id )
+        .Include(u => u.CurrentStage)
+        .Include(u => u.CurrentStage)
+        .FirstOrDefault();
+
+        return new ServiceResult<Meditator>
+        {
+            Success = true,
+            Data = user!
+        };
+    }
+
+    private async Task<string> SaveFileAsync(IFormFile file)
+    {
+        if (!Directory.Exists(_storagePath))
+        {
+            Directory.CreateDirectory(_storagePath);
+        }
+
+        var fileName = Path.GetRandomFileName() + Path.GetExtension(file.FileName);
+        var filePath = Path.Combine(_storagePath, fileName);
+
+        using (var stream = new FileStream(filePath, FileMode.Create))
+        {
+            await file.CopyToAsync(stream);
+        }
+
+        return $"/uploads/{fileName}";
     }
 }

37c0f10a633d0947c6b7ed8205f7c8c27a4d6e0c
Abenezer Walelign <walelignabenezer@gmail.com>
2024-06-16 17:34:42 +0300

    Made all fields in UpdateUserAccountDTO nullable except for the ID

--- backend/DTOs/User/UpdateUserAccountDTO.cs
+++ backend/DTOs/User/UpdateUserAccountDTO.cs
@@ -1,8 +1,8 @@
 public class UpdateUserAccountDTO
 {
     public int Id { get; set; }
-    public IFormFile ProfilePicture { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Username { get; set; }
+    public IFormFile? ProfilePicture { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Username { get; set; }
 }

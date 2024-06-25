86844d0cc02700962544598d1353004a79b37bb9
Abenezer Walelign <walelignabenezer@gmail.com>
2024-06-25 22:14:25 +0300

    Added UserAccountReducer to store

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,8 +1,9 @@
 import {configureStore} from "@reduxjs/toolkit";
 import {thunk} from "redux-thunk";
 import AuthReducer from "../slices/AuthSlice";
+import UserAccountReducer from "../slices/UserSlice";
 
 export const store = configureStore({
-  reducer: {Auth: AuthReducer},
+  reducer: {Auth: AuthReducer, Account: UserAccountReducer},
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
 });

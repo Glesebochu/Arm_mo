26a95d4997306b685ac68741115a341bf2ce2dff
Fellasfaw <fellasfaw@outlook.com>
2024-07-01 14:58:36 +0300

    Boldened the main portion of the instruction.

--- frontend/src/components/TransitionSteps/Step1Box.jsx
+++ frontend/src/components/TransitionSteps/Step1Box.jsx
@@ -10,7 +10,7 @@ export function Step1Box({ meditationObjectType, onDone }) {
         <Card className="step-box w-full h-full flex flex-col items-center justify-center p-6 bg-white shadow-lg rounded-lg">
 
             <h3 className="text-2xl text-center m-10">
-                Relax and be open to all <span className='text-primary p-1'>{textReplacement}</span> in this moment.
+                Relax and be open to all <span className='text-primary font-semibold p-1'>{textReplacement}</span> in this moment.
                 <br />
                 Take in everything as objectively as possible - as it is.
             </h3>

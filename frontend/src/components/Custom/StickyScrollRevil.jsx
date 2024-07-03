5c6abb73e83462593108a6f2d45afcc403c1feb2
Abenezer Walelign <walelignabenezer@gmail.com>
2024-07-03 22:31:00 +0300

    Center-aligned feature section description and title for small and medium screens

--- frontend/src/components/Custom/StickyScrollRevil.jsx
+++ frontend/src/components/Custom/StickyScrollRevil.jsx
@@ -47,10 +47,10 @@ export const StickyScroll = ({
       className="h-[28rem] w-[1150px] overflow-y-auto no-scrollbar flex justify-center relative space-x-10 rounded-md p-4 scrollbar-hide"
       ref={ref}
     >
-      <div className="div relative flex items-start px-4 flex-1">
+      <div className="div relative flex items-start px-4 flex-1 justify-center">
         <div className="max-w-2xl">
           {content.map((item, index) => (
-            <div key={item.title + index} className="my-8">
+            <div key={item.title + index} className="my-8 w-full">
               <motion.h2
                 initial={{
                   opacity: 0,
